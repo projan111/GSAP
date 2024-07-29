e06ca4baa177d16ec527ee784a595192bd9541c2
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-29 20:52:36 +0545

    add pin in gsap

--- app.js
+++ app.js
@@ -17,30 +17,42 @@
 //   },
 // });
 
+// gsap.from("#page2 h1", {
+//   opacity: 0,
+//   duration:2,
+//   x:-400,
+//   scrollTrigger:{
+//     trigger:"#page2 h1",
+//     scroller: "body",
+//     markers:true,
+//     start: "top 60%",
+//     scrub: 1,
+//     pin:true
+//   }
+// })
+// gsap.from("#page2 h2", {
+//   opacity: 0,
+//   duration:2,
+//   x:400,
+//   scrollTrigger:{
+//     trigger:"#page2 h2",
+//     scroller: "body",
+//     markers:true,
+//     start: "top 60%",
+//     scrub: 1,
+//     pin:true
+//   }
+// })
 
-gsap.from("#page2 h1", {
-  opacity: 0,
-  duration:2,
-  x:-400,
-  scrollTrigger:{
-    trigger:"#page2 h1",
+gsap.to("#page2 h1", {
+  transform: "translateX(-150%)",
+  scrollTrigger: {
+    trigger:"#page2",
     scroller: "body",
-    markers:true,
-    start: "top 60%",
-    scrub: 1,
-    pin:true
+    markers: true,
+    start: "top 0%",
+    end:"top -100",
+    pin:true,
+    scrub:2,
   }
-})
-gsap.from("#page2 h2", {
-  opacity: 0,
-  duration:2,
-  x:400,
-  scrollTrigger:{
-    trigger:"#page2 h2",
-    scroller: "body",
-    markers:true,
-    start: "top 60%",
-    scrub: 1,
-    pin:true
-  }
-})
+});
