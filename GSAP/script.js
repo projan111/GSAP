bea34651a2e0166113dc39c4a109b0a31833ffb9
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2025-07-25 11:34:51 +0545

    add timeline

--- GSAP/script.js
+++ GSAP/script.js
@@ -26,3 +26,51 @@ gsap.from(".font h1", {
   duration: 1,
   delay: 1,
 });
+
+gsap.to(".tool1", {
+  x: 1000,
+  rotate: 360,
+  duration: 1.5,
+  delay: 1,
+});
+
+gsap.to(".tool2", {
+  x: 1000,
+  duration: 1.5,
+  delay: 2.5,
+  backgroundColor: "yellow",
+
+  // repeat: 1, // Repeat one more time
+  // yoyo: true,
+});
+
+gsap.to(".tool3", {
+  x: 1000,
+  rotate: 360,
+  duration: 1.5,
+  scale: 0.5,
+  borderRadius: "50%",
+  delay: 4,
+});
+
+var tl = gsap.timeline();
+
+tl.from(".logo", {
+  y: -20,
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.5,
+});
+
+tl.from("h2", {
+  y: -20,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+});
+
+tl.from(".hero-w", {
+  opacity: 0,
+  duration: 1,
+  scale: 0.2,
+});
